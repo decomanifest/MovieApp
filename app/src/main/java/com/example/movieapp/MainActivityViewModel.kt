4e73635aab4e8fa1a4893773a23b9b95d778c7ea
kamil kasprzyk <>
2022-06-07 14:50:12 +0200

    add simple debounce option

--- app/src/main/java/com/example/movieapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieapp/MainActivityViewModel.kt
@@ -8,6 +8,8 @@ import com.example.movieapp.data.reponse.MovieEntity
 import com.example.movieapp.domain.usecase.GetMovieList
 import com.example.movieapp.helpers.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,6 +21,8 @@ class MainActivityViewModel @Inject constructor(
 ) : ViewModel() {
     private val _moviesList: MutableLiveData<List<MovieEntity>> = MutableLiveData()
     val moviesList: LiveData<List<MovieEntity>> = _moviesList
+    private var searchJob: Job? = null
+    private var debounceJob: Job? = null
 
     fun searchMovies(query: String) {
         if (query.isBlank()) {
@@ -26,15 +30,23 @@ class MainActivityViewModel @Inject constructor(
             return
         }
 
-        fetchMovies(query)
+        debounceJob?.cancel()
+        searchJob?.cancel()
+        debounceJob = viewModelScope.launch {
+            delay(DEBOUNCE_PERIOD)
+            searchJob = fetchMovies(query)
+        }
     }
 
-    private fun fetchMovies(query: String) {
+    private fun fetchMovies(query: String) =
         viewModelScope.launch(dispatcherProvider.main) {
             runCatching {
                 val result = getMovieList.executeAction(GetMovieList.Params(query), dispatcherProvider.io)
                 _moviesList.value = result
             }.onFailure { Timber.d("Fetching error ${it.message}") }
         }
+
+    companion object {
+        private const val DEBOUNCE_PERIOD = 300L
     }
 }
